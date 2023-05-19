4dc727551820067185e4e5e3b568c21c82796250
ToshaEX <tharinduoshan@gmail.com>
2023-05-19 13:47:07 +0530

    dep

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   app.enableCors();
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  await app.listen(5000);
+  await app.listen(parseInt(process.env.PORT) || 8080);
 }
 bootstrap();
