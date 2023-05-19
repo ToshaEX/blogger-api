e6f6976610af47a987448640beeedfb1a91b915d
ToshaEX <tharinduoshan@gmail.com>
2023-05-19 12:44:50 +0530

    fixing cors error

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,15 @@ import * as bodyParser from 'body-parser';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'uploads'));
   app.useGlobalPipes(new ValidationPipe());
-  // app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000/', // Specify the allowed origin(s) here
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept,Authorization',
+  });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(8080);
